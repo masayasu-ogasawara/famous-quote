3030ac357ca65687b2bfc7bbe9dc85d60c46b1a0
masayasu-ogasawara <ogasawara.masayasu@gmail.com>
2020-08-03 17:42:15 +0900

    edit

--- app/controllers/famous_controller.rb
+++
@@ -1,4 +0,0 @@
-class FamousController < ApplicationController
-  def index
-  end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users
-  get 'famous/index'
-  root 'famous#index'
+  # devise_for :users
+  # root 'words#index'
+  # resources :words
+  resource :user, only: [:show, :edit, :update]
+  resources :users, only: [:index, :show]
+
 end
